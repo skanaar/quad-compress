bcd621674d2d8fee04d01e10895aeb1c242f2891
skanaar <daniel.kallin@gmail.com>
2020-04-21 00:03:52 +0200

    quad tree area quantization

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,83 @@
 use std::cmp::{min, max};
-use image::{ DynamicImage, Pixel };
+use image::{ RgbImage, ImageBuffer, DynamicImage, Pixel };
 use image::error::ImageResult;
 
 enum Quadtree {
     Leaf(i32),
-    Branch(Box<Quadtree>, Box<Quadtree>, Box<Quadtree>, Box<Quadtree>)
+    Branch(i32, i32, Box<Quadtree>, Box<Quadtree>, Box<Quadtree>, Box<Quadtree>),
+    Root(u32, Box<Quadtree>),
 }
 
 impl Quadtree {
+    fn construct(input: &Vec<i32>) -> Box<Quadtree> {
+        let rank = (input.len() as f32).sqrt() as usize;
+        fn build(data: &Vec<i32>, p: (usize, usize), window: usize, rank: usize) -> Box<Quadtree> {
+            if window == 1 {
+                return Box::new(Quadtree::Leaf(data[p.0 + p.1*rank]))
+            }
+            let s = window/2;
+            let a = build(data, (p.0, p.1), s, rank);
+            let b = build(data, (p.0+s, p.1), s, rank);
+            let c = build(data, (p.0, p.1+s), s, rank);
+            let d = build(data, (p.0+s, p.1+s), s, rank);
+            return Box::new(Quadtree::Branch(
+                min(min((*a).min(), (*b).min()), min((*c).min(), (*d).min())),
+                max(max((*a).max(), (*b).max()), max((*c).max(), (*d).max())),
+                a, b, c, d,
+            ));
+        }
+        assert!(input.len() == rank * rank);
+        return Box::new(Quadtree::Root(rank as u32, build(input, (0, 0), rank, rank)));
+    }
     fn min(&self) -> i32 {
         match self {
             Quadtree::Leaf(value) => *value,
-            Quadtree::Branch(a, b, c, d) => min(min(a.min(), b.min()), min(c.min(), d.min())),
+            Quadtree::Branch(min, _, _, _, _, _) => *min,
+            Quadtree::Root(_, data) => data.min(),
         }
     }
     fn max(&self) -> i32 {
         match self {
             Quadtree::Leaf(value) => *value,
-            Quadtree::Branch(a, b, c, d) => max(max(a.max(), b.max()), max(c.max(), d.max())),
+            Quadtree::Branch(_, max, _, _, _, _) => *max,
+            Quadtree::Root(_, data) => data.max(),
         }
     }
     fn contrast(&self) -> i32 {
+        return self.max() - self.min();
+    }
+}
+
+impl Quadtree {
+    fn get(&self, x: u32, y: u32, offset_x: u32, offset_y: u32, window: u32) -> i32 {
         match self {
-            Quadtree::Leaf(_) => 0,
-            Quadtree::Branch(_, _, _, _) => self.max() - self.min(),
+            Quadtree::Leaf(value) => *value,
+            Quadtree::Branch(min, max, a, b, c, d) => {
+                if self.contrast() < 20 {
+                    return (max + min) / 2;
+                } else {
+                    let s = window/2;
+                    let left = (x - offset_x) < s;
+                    let top = (y - offset_y) < s;
+                    match (left, top) {
+                        (true, true) => a.get(x, y, offset_x, offset_y, s),
+                        (false, true) => b.get(x, y, offset_x +  s, offset_y, s),
+                        (true, false) => c.get(x, y, offset_x, offset_y + s, s),
+                        (false, false) => d.get(x, y, offset_x + s, offset_y + s, s),
+                    }
+                }
+            },
+            Quadtree::Root(_, data) => data.get(x, y, offset_x, offset_y, window),
         }
     }
-    fn serialize(&self, buffer: &mut Vec<i32>) {
+    fn to_image(&self) -> RgbImage {
         match self {
-            Quadtree::Leaf(value) => {
-                buffer.push(0i32);
-                buffer.push(*value);
-            },
-            Quadtree::Branch(a, b, c, d) => {
-                buffer.push(1i32);
-                a.serialize(buffer);
-                b.serialize(buffer);
-                c.serialize(buffer);
-                d.serialize(buffer);
-            },
-        }
-    }
-    fn construct(root: &Vec<i32>) -> Box<Quadtree> {
-        fn build(data: &Vec<i32>, p: (usize, usize), window: usize) -> Box<Quadtree> {
-            if window == 1 {
-                return Box::new(Quadtree::Leaf(data[p.0 + p.1*window]))
-            }
-            let s = window/2;
-            return Box::new(Quadtree::Branch(
-                build(data, (p.0, p.1), s),
-                build(data, (p.0+s, p.1), s),
-                build(data, (p.0+s, p.1+s), s),
-                build(data, (p.0, p.1+s), s),
-            ));
-        }
-        let rank = (root.len() as f32).sqrt() as usize;
-        assert!(root.len() == rank * rank);
-        return build(root, (0, 0), rank);
-    }
-    fn describe(&self, depth: usize) {
-        match self {
-            Quadtree::Leaf(value) => {
-                print!("{} ", value);
-            },
-            Quadtree::Branch(a, b, c, d) => {
-                println!("");
-                print!("{} quad {}-{} : ", " ".repeat(depth * 2), self.min(), self.max());
-                a.describe(depth+1);
-                b.describe(depth+1);
-                c.describe(depth+1);
-                d.describe(depth+1);
-            },
+            Quadtree::Leaf(_) => panic!(),
+            Quadtree::Branch(_, _, _, _, _, _) => panic!(),
+            Quadtree::Root(rank, data) => ImageBuffer::from_fn(*rank, *rank, |x, y| {
+                let c = data.get(x, y, 0, 0, *rank) as u8;
+                image::Rgb([c, c, c])
+            }),
         }
     }
 }
@@ -95,18 +102,10 @@ fn main() {
     // ];
     //let x = Quadtree::construct(&v);
     let x = analyze_img(image::open("./lena.png"));
+    let res = x.to_image().save("./output.png");
+    println!("saved image: {}", res.is_ok());
 
     println!("contrast: {}", x.contrast());
     println!("min: {}", x.min());
     println!("max: {}", x.max());
-    //x.describe(0);
-
-    // println!("");
-    // println!("serialization:");
-    // let mut data = Vec::new();
-    // x.serialize(&mut data);
-    // for item in data.iter() {
-    //     print!("{}", item);
-    // }
-    // println!("");
 }
